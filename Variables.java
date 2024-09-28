7179c78bad33e95c9c8a6409c72a55862876bf49
Guillermo Home <guille.work.94@gmail.com>
2024-09-28 11:04:58 -0400

    Primer commit

---
+++ Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaración de variables
+        int edad;
+        double altura;
+        boolean esEstudiante;
+        char inicial;
+        String nombreCompleto;
+ 
+        // Asignación de valores a las variables
+        edad = 25;
+        altura = 1.75;
+        esEstudiante = true;
+        inicial = 'J';
+        nombreCompleto = "Juan Pérez";
+ 
+        // Imprimir los valores de las variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Es estudiante: " + esEstudiante);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Nombre completo: " + nombreCompleto);
+    }
+ }
